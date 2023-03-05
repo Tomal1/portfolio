ba34e3a6a9792b3eca2b38867d45884fb5913a5f
Minhazul Islam <minhazulislam@hotmail.com>
2023-03-05 21:53:55 +0000

    thumbnail images added to portfolio section

--- src/components/Pages/Portfolio/index.js
+++ src/components/Pages/Portfolio/index.js
@@ -4,10 +4,10 @@ import "./portfolio.css";
 const Portfolio = () => {
     return(
         <div className="tumbnailCon">
-            <div className="tumbnails"></div>
-            <div className="tumbnails"></div>
-            <div className="tumbnails"></div>
-            <div className="tumbnails"></div>
+            <div className="tumbnails thumb1"></div>
+            <div className="tumbnails thumb2"></div>
+            <div className="tumbnails thumb3"></div>
+            <div className="tumbnails thumb4"></div>
         </div>
     )
 }
