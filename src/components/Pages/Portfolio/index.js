c5fb8e573e05b0fc705ecb923e327af551b410dc
Minhazul Islam <minhazulislam@hotmail.com>
2023-03-14 17:10:36 +0000

    contents postioning adjusted and 2 buttons under each thumbnail added

--- src/components/Pages/Portfolio/index.js
+++ src/components/Pages/Portfolio/index.js
@@ -1,13 +1,53 @@
 import React from "react";
 import "./portfolio.css";
 
+import image1 from "../../../assets/images/codeQuiz.png";
+import image2 from "../../../assets/images/noteScreenshot.png";
+import image3 from "../../../assets/images/passwordScreenshot.png";
+import image4 from "../../../assets/images/rpsScreenshot.png";
+import image5 from "../../../assets/images/WDS.png";
+import image6 from "../../../assets/images/weatherScreenshot.png";
+
+
+
 const Portfolio = () => {
     return(
         <div className="tumbnailCon">
-            <div className="tumbnails thumb1"></div>
-            <div className="tumbnails thumb2"></div>
-            <div className="tumbnails thumb3"></div>
-            <div className="tumbnails thumb4"></div>
+            <div className="tumbnails">
+                <img src={image1} className="thumb1 images"/>
+                <input className="portoBTN" type="button" value="website"></input>
+                <input className="portoBTN" type="button" value="github"></input>
+            </div>
+            <div className="tumbnails">
+                <img src={image2} className="thumb2 images"/>
+                <input className="portoBTN" type="button" value="website"></input>
+                <input className="portoBTN" type="button" value="github"></input>
+
+            </div>
+            <div className="tumbnails">
+                <img src={image3} className="thumb3 images"/>
+                <input className="portoBTN" type="button" value="website"></input>
+                <input className="portoBTN" type="button" value="github"></input>
+
+            </div>
+            <div className="tumbnails">
+                <img src={image4} className="thumb4 images"/>
+                <input className="portoBTN" type="button" value="website"></input>
+                <input className="portoBTN" type="button" value="github"></input>
+
+            </div>
+            <div className="tumbnails">
+                <img src={image5} className="thumb5 images"/>
+                <input className="portoBTN" type="button" value="website"></input>
+                <input className="portoBTN" type="button" value="github"></input>
+
+            </div>
+            <div className="tumbnails">
+                <img src={image6} className="thumb6 images"/>
+                <input className="portoBTN" type="button" value="website"></input>
+                <input className="portoBTN" type="button" value="github"></input>
+
+            </div>
         </div>
     )
 }
